9a545eb6a127e4d2c0782dc35b370bb952955a9d
Mridul Sharma <mridul.sharma1@ontariotechu.net>
2025-04-02 20:30:45 -0400

    Updated account icon with sign in and admin sign in

--- resources/scripts/navbar.js
+++ resources/scripts/navbar.js
@@ -123,15 +123,41 @@ class NavbarComponent extends HTMLElement {
   }
 
   updateAccountBadge() {
-    // This method updates the account dropdown based on login state.
+    // Select the account badge anchor and the dropdown container by ID.
+    const accountAnchor = this.querySelector('#accountBadge');
     const accountDropdown = this.querySelector('#accountDropdown');
-    if (!accountDropdown) {
-      console.error("Account dropdown not found in navbar.");
-      return;
-    }
+    
+    // Check if a user is logged in by reading localStorage.
     const loggedInUser = localStorage.getItem('loggedInUser');
+    const isAdmin = localStorage.getItem('isAdmin') === 'true';
+  
     if (loggedInUser) {
-      // If user is logged in, show only "Sign Out"
+      let newIconSVG = '';
+      if (isAdmin) {
+        // Admin icon: person-fill-gear.
+        newIconSVG = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" 
+               class="bi bi-person-fill-gear hover-effect" viewBox="0 0 16 16">
+            <path d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+            <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+            <path d="M11.5 13a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+            <path d="M11.5 12a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z"/>
+            <path d="M13.405 11.32a.5.5 0 0 0-.611-.21l-.645.258a2.5 2.5 0 0 0-.48-.278l-.097-.68a.5.5 0 0 0-.5-.436h-1.29a.5.5 0 0 0-.5.436l-.097.68a2.5 2.5 0 0 0-.48.278l-.645-.258a.5.5 0 0 0-.611.21l-.39.674a.5.5 0 0 0 .122.617l.516.4c-.028.157-.042.317-.042.48 0 .163.014.323.042.48l-.516.4a.5.5 0 0 0-.122.617l.39.674a.5.5 0 0 0 .611.21l.645-.258c.15.11.312.203.48.278l.097.68a.5.5 0 0 0 .5.436h1.29a.5.5 0 0 0 .5-.436l.097-.68a2.5 2.5 0 0 0 .48-.278l.645.258a.5.5 0 0 0 .611-.21l.39-.674z"/>
+          </svg>
+        `;
+      } else {
+        // Regular logged in user: person-fill-check.
+        newIconSVG = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" 
+               class="bi bi-person-fill-check hover-effect" viewBox="0 0 16 16">
+            <path d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+            <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+            <path fill="none" stroke="currentColor" stroke-width="1" d="M6 9l2 2 4-4"/>
+          </svg>
+        `;
+      }
+      accountAnchor.innerHTML = newIconSVG;
+      // Update the dropdown to show a single "Sign Out" option.
       accountDropdown.innerHTML = `
         <li>
           <a class="dropdown-item" href="#" id="signOutButton">Sign Out</a>
@@ -162,7 +188,14 @@ class NavbarComponent extends HTMLElement {
         });
       }
     } else {
-      // If user is not logged in, show "Sign Up" and "Sign In"
+      // No user is logged in: revert to default icon and login options.
+      accountAnchor.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" 
+             class="bi bi-person-circle hover-effect" viewBox="0 0 16 16">
+          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+          <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+        </svg>
+      `;
       accountDropdown.innerHTML = `
         <li>
           <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#signUpModal">Sign Up</a>
@@ -172,7 +205,7 @@ class NavbarComponent extends HTMLElement {
         </li>
       `;
     }
-  }
+  }  
 }
 
 customElements.define('navbar-component', NavbarComponent);
